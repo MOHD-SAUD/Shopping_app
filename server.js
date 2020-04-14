3ba7eac671f737b98838188a1d92ee0bac71844d
Mohd Saud <mohdsaud2010@gmail.com>
2020-04-14 08:57:41 +0530

    Specified details in package.json
    Signed-off-by: Mohd Saud <mohdsaud2010@gmail.com>

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.use('/add',express.static(__dirname+'/public/add_product.html'));
 app.use('/api',require('./routes/api').route)
 app.use('/add/view',express.static(__dirname+'/public'));
 
-app.listen(2233,()=>{
+app.listen(PORT,()=>{
     console.log("server started at http://localhost:2233")
 })
